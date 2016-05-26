dd979d495deef84b7f3abd3871476ff1002cb6bf
rboyko <you@example.com>
2016-05-26 17:00:36 +0300

    Mod main activity

--- app/src/main/java/cherkassy/pro/alphabet/MainActivity.java
+++ app/src/main/java/cherkassy/pro/alphabet/MainActivity.java
@@ -1,18 +1,42 @@
 package cherkassy.pro.alphabet;
 
+import android.support.v4.view.LayoutInflaterCompat;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Layout;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.TableLayout;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView image_a,image_b;
+    FrameLayout frame_1;
+    TableLayout tableLayout_1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        LayoutInflater inflater=getLayoutInflater();
+        android.view.View layout=inflater.inflate(R.layout.activity_main, null);
+        image_a=(ImageView)layout.findViewById(R.id.image_a);
+        image_b=(ImageView)layout.findViewById(R.id.image_a);
+        frame_1=(FrameLayout)layout.findViewById(R.id.frame_1);
+        tableLayout_1=(TableLayout)layout.findViewById(R.id.table_layout_1);
+
+        setContentView(layout);
+
+        Log.v("MYVARN",frame_1.getWidth()+"x"+frame_1.getHeight());
+        Log.v("MYVARN",tableLayout_1.getWidth()+"x"+tableLayout_1.getHeight());
+
+
     }
 
 
@@ -36,5 +60,8 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private class View {
     }
 }
